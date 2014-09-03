e1dc119270585d9ed0c5949694ff7a812784c95e
Caleb Stauffer <develop+bitbucket@calebstauffer.com>
2014-09-03 10:36:56 -0400

    new options: same template, and jump

--- admin.js
+++ admin.js
@@ -11,10 +11,15 @@ jQuery(function($) {
 	});
 
 	$("#misc-pub-andthen-select > ul > li > input[type=radio]").click(function() {
-		$("#misc-pub-andthen-select .andthen-add-options").hide();
+		$("#misc-pub-andthen-select > ul > li .andthen-options").slideUp(200);
+		$(this).parent().find('.andthen-options').slideDown(200);
 
-		if ($("input#andthen-add-order").is(':checked')) $("#misc-pub-andthen-select .andthen-add-options-order").slideDown(200);
-		else $("#misc-pub-andthen-select .andthen-add-options-order").slideUp(200);
+		/*$("#misc-pub-andthen-select .andthen-add-options").hide();
+
+		if ($("input#andthen-add-order").is(':checked'))
+			$("#misc-pub-andthen-select .andthen-add-options-order").slideDown(200);
+		else
+			$("#misc-pub-andthen-select .andthen-add-options-order").slideUp(200);*/
 	});
 
 	$("input#andthen-action-add").click(function() {
@@ -26,6 +31,25 @@ jQuery(function($) {
 		}
 	});
 
+	$("input#andthen-action-goto").click(function() {
+		var opts = $(this).parent().find('.andthen-goto-options');
+		var pt = $("input#post_type").val();
+		if (pt != $("select#andthen-goto").find('option:selected').val()) {
+			$("#misc-pub-andthen-select .andthen-goto-options").slideUp(200,function() {
+				$("#misc-pub-andthen-select .andthen-goto-options input[type=checkbox]").prop('checked','');
+			});
+		} else $("#misc-pub-andthen-select .andthen-goto-options").slideDown(200);
+	});
+
+		$("select#andthen-goto").on('change',function() {
+			var pt = $("input#post_type").val();
+			if (pt != $(this).find('option:selected').val()) {
+				$("#misc-pub-andthen-select .andthen-options-goto").slideUp(200,function() {
+					$("#misc-pub-andthen-select .andthen-options-goto input[type=checkbox]").prop('checked','');
+				});
+			} else $("#misc-pub-andthen-select .andthen-options-goto").slideDown(200);
+		});
+
 	$("#andthen").on('change',"select#andthen-add",function() {
 		var pt = $("input#post_type").val();
 		if (pt != $(this).find('option:selected').val()) {
@@ -36,8 +60,22 @@ jQuery(function($) {
 	});
 
 	$("#pageparentdiv").on('change',"select#parent_id",function() {
-		if ('' == $(this).find('option:selected').val()) $("#andthen .andthen-add-options-parent").slideUp(200,function() { $(this).prop('checked',''); });
-		else $("#andthen .andthen-add-options-parent").slideDown(200);
+		if ('' == $(this).find('option:selected').val())
+			$("#andthen .andthen-add-options-parent").slideUp(200,function() {
+				$(this).prop('checked','');
+			});
+		else
+			$("#andthen .andthen-add-options-parent").slideDown(200);
+	}).on('change',"#page_template",function() {
+		if ('' == $(this).find('option:selected').val() || 'default' == $(this).find('option:selected').val())
+			$("#andthen .andthen-add-options-template").slideUp(200,function() {
+				if ($("#andthen-add-template").is(':checked')) {
+					$("#andthen-add-template").click();
+					$("#andthen .save-post-andthen").click();
+				}
+			});
+		else
+			$("#andthen .andthen-add-options-template").slideDown(200);
 	});
 
 	/*$("input#andthen-add-order").click(function() {
@@ -58,17 +96,21 @@ jQuery(function($) {
 		$("#andthen-action").val(checked.val());
 
 		if ('add' == checked.val()) {
-			display = 'Add new ';
+			display = 'New ';
+			if ($("#andthen-add-template").is(':checked')) display += 'templated ';
 			if ($("#andthen-add-parent").is(':checked')) display += 'child ';
 			display += $("#andthen-add > option:selected").html().toLowerCase();
 			if ($("#andthen-add-order").is(':checked')) {
 				if ('increment' == $("#andthen-add-order-turn").val()) display += '++';
 				else display += '--';
 			}
-		} else if ('goto' == checked.val())
+		} else if ('goto' == checked.val()) {
 			display = 'Go to ' + $("#andthen-goto > option:selected").html();
+			if ($("#andthen-goto-position").is(':checked'))
+				display += ' (jump)';
+		}
 
-		$("#andthen #andthen-display").html(display);
+		$("#andthen #andthen-add-display").html(display);
 	});
 
 	$("#andthen .cancel-post-andthen").click(function(ev) {
--- andthen.php
+++ andthen.php
@@ -38,12 +38,15 @@ class css_andthen {
 
 		$meta = array('action' => $_POST['andthen']['action']);
 		if ('add' == $_POST['andthen']['action']) {
-			if (isset($_POST['andthen']['add'])) $meta['add'] = $_POST['andthen']['add'];
-			if (isset($_POST['andthen']['parent'])) $meta['parent'] = 1;
-			if (isset($_POST['andthen']['order'])) $meta['order'] = 1;
-			if (isset($_POST['andthen']['orderdir'])) $meta['orderdir'] = $_POST['andthen']['orderdir'];
-		} else if ('goto' == $_POST['andthen']['action'])
+			if (isset($_POST['andthen']['add']))		$meta['add'] = $_POST['andthen']['add'];
+			if (isset($_POST['andthen']['parent']))		$meta['parent'] = 1;
+			if (isset($_POST['andthen']['template']))	$meta['template'] = 1;
+			if (isset($_POST['andthen']['order']))		$meta['order'] = 1;
+			if (isset($_POST['andthen']['orderdir']))	$meta['orderdir'] = $_POST['andthen']['orderdir'];
+		} else if ('goto' == $_POST['andthen']['action']) {
 			$meta['goto'] = $_POST['andthen']['goto'];
+			$meta['position'] = $_POST['andthen']['position'];
+		}
 
 		$user = get_current_user_id();
 		if (get_user_meta($user,'publish-andthen',true) != $meta)
@@ -69,7 +72,7 @@ class css_andthen {
 		$this->labels = array(
 			'edit'	=> 'Edit',
 			'view'	=> 'View',
-			'add'	=> 'Add new',
+			'add'	=> 'New',
 			'goto'	=> 'Go to',
 		);
 
@@ -99,8 +102,12 @@ class css_andthen {
 
 		function load_new() {
 			global $post;
-			if (isset($_GET['parent_id'])) $post->post_parent = $_GET['parent_id'];
-			if (isset($_GET['menu_order'])) $post->menu_order = $_GET['menu_order'];
+			if (isset($_GET['parent_id']))	$post->post_parent		= $_GET['parent_id'];
+			if (isset($_GET['menu_order']))	$post->menu_order		= $_GET['menu_order'];
+			if (isset($_GET['template'])) {
+				$previous = get_post($_GET['template']);
+				$post->page_template = $previous->page_template;
+			}
 
 			//echo '<pre>' . print_r($this->usermeta,true) . '</pre>';
 		}
@@ -124,6 +131,7 @@ class css_andthen {
 			$url = admin_url('post-new.php');
 			$url = add_query_arg('post_type',$andthen['add'],$url);
 			if (isset($andthen['parent'])) $url = add_query_arg('parent_id',$post['parent_id'],$url);
+			if (isset($andthen['template'])) $url = add_query_arg('template',$post_id,$url);
 			if (isset($andthen['order'])) {
 				$order = $post['menu_order'];
 				if ('increment' == $andthen['orderdir']) $order++;
@@ -133,6 +141,8 @@ class css_andthen {
 		} else if ('goto' == $this->action) {
 			if (is_array($this->goto[$andthen['goto']])) $url = $this->goto[$andthen['goto']][1];
 			else $url = $this->goto[$andthen['goto']];
+			if (isset($post['andthen']['position']))
+				$url .= '#post-' . $post_id;
 		}
 
 		if (isset($args['message'])) $url = add_query_arg('message',$args['message'],$url);
@@ -164,8 +174,9 @@ class css_andthen {
 		global $post;
 		$this->post_type = $post->post_type;
 
-		$add = $child = $order = $orderdir = $goto = '';
+		$add = $templated = $child = $order = $orderdir = $goto = '';
 		if (isset($this->usermeta['add']) && 'add' == $this->usermeta['action'])				$add = ' ' . strtolower($this->post_types[$this->post_type]->labels->singular_name);
+		if (isset($this->usermeta['template'])) $templated = ' templated';
 		if (isset($this->usermeta['parent']) && is_post_type_hierarchical($this->post_type))	$child = ' child';
 		if (isset($this->usermeta['order']) && isset($this->usermeta['orderdir'])) {
 			if ('increment' == $this->usermeta['orderdir']) $order = '++';
@@ -176,11 +187,13 @@ class css_andthen {
 			if (is_array($this->goto[$this->usermeta['goto']]))
 				list($goto,$url) = $this->goto[$this->usermeta['goto']];
 			else $goto = ucfirst($this->usermeta['goto']);
+			if (isset($this->usermeta['position']))
+				$goto .= ' (jump)';
 		}
 
 		if ('edit' == $this->action)		$display = 'Edit this ' . strtolower($this->post_types[$this->post_type]->labels->singular_name);
 		else if ('view' == $this->action)	$display = 'View this ' . strtolower($this->post_types[$this->post_type]->labels->singular_name);
-		else if ('add' == $this->action)	$display = 'Add new' . $child . $add . $order;
+		else if ('add' == $this->action)	$display = 'New' . $templated . $child . $add . $order;
 		else if ('goto' == $this->action)	$display = 'Go to ' . $goto;
 
 		//echo '<pre>' . print_r($this->post_types['post'],true) . '</pre>';
@@ -189,7 +202,7 @@ class css_andthen {
 		<div class="misc-pub-section misc-pub-andthen" id="andthen">
 
 			<span class="title">And Then:</span> 
-			<span id="andthen-display"><?php echo $display ?></span> 
+			<span id="andthen-add-display"><?php echo $display ?></span> 
 			<a href="#andthen" id="andthen-edit" class="edit-andthen hide-if-no-js"><span aria-hidden="true">Edit</span><span class="screen-reader-text">Edit andthen action</span></a>
 
 			<div id="misc-pub-andthen-select" class="hide-if-no-js" style="display: none;">
@@ -215,17 +228,26 @@ class css_andthen {
 					<li class="andthen-add">
 
 						<input type="radio" id="andthen-action-add" name="andthen-action" value="add" <?php checked('add',$this->action,true) ?> />
-						<label for="andthen-action-add">Add new:</label> <?php $this->addnew() ?>
+						<label for="andthen-action-add">New:</label> <?php $this->addnew() ?>
 
 						<?php if ($hier = is_post_type_hierarchical($this->post_type) || (1 == $this->post_types[$this->post_type]->_builtin || $attr = post_type_supports($this->post_type,'page-attributes'))) { ?>
 
-							<ul class="andthen-add-options"<?php if ('add' != $this->action) echo ' style="display: none;"' ?>>
+							<ul class="andthen-options andthen-options-add"<?php if ('add' != $this->action) echo ' style="display: none;"' ?>>
 
 								<?php if ($hier) { ?>
 
 									<li class="andthen-add-options-parent"<?php if (in_array($post->post_parent,array('',0))) echo ' style="display: none;"' ?>>
 										<input type="checkbox" id="andthen-add-parent" name="andthen[parent]" value="1"<?php checked(1,isset($this->usermeta['parent']),true) ?> />
 										<label for="andthen-add-parent"> Same parent</label>
+									</li>
+
+								<?php } ?>
+
+								<?php if ('page' == $this->post_type) { ?>
+
+									<li class="andthen-add-options-template"<?php if (in_array($post->page_template,array(false,'default'))) echo ' style="display: none;"' ?>>
+										<input type="checkbox" id="andthen-add-template" name="andthen[template]" value="1"<?php checked(1,isset($this->usermeta['template']),true) ?> />
+										<label for="andthen-add-template"> Same template</label>
 									</li>
 
 								<?php } ?>
@@ -257,6 +279,15 @@ class css_andthen {
 
 						<input type="radio" id="andthen-action-goto" name="andthen-action" value="goto" <?php checked('goto',$this->action,true) ?> />
 						<label for="andthen-action-goto"> Go to:</label> <?php $this->goes() ?>
+
+						<ul class="andthen-options andthen-options-goto"<?php if ('goto' != $this->action) echo ' style="display: none;"' ?>>
+
+							<li class="andthen-goto-options-position">
+								<input type="checkbox" id="andthen-goto-position" name="andthen[position]" value="1"<?php checked(1,isset($this->usermeta['position']),true) ?> />
+								<label for="andthen-goto-position"> Jump to edited <?php echo $this->post_types[$this->post_type]->name ?></label>
+							</li>
+
+						</ul>
 
 					</li>
 				
